0eb16561825a0cacd7977b3eadc5bc942b280710
bstocker <boris.stocker@gmail.com>
2024-02-11 13:50:00 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template_string, render_template, jsonify
 from flask import render_template
 from flask import json
+from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
@@ -9,6 +10,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
-                                                                                                                                       
+
+@app.route('/extract-minutes/<date_string>')
+def extract_minutes(date_string):
+        date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
+        minutes = date_object.minute
+        return jsonify({'minutes': minutes})
+  
 if __name__ == "__main__":
   app.run(debug=True)
