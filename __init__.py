79c954e4ff74b266486c425b9855f4f4da10753b
Yann <137297146+yyspos@users.noreply.github.com>
2025-01-09 10:44:21 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -31,9 +31,7 @@ def meteo():
 def mongraphique():
     return render_template("graphique.html")
 
-@app.route("/histogramme/")
-def mongraphique():
-    return render_template("graphique.html")
+
   
 if __name__ == "__main__":
   app.run(debug=True)
